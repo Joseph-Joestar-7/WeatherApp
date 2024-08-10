7a830c81021974fb202dbe94e6cc4ed47c5237f5
Ritaban Chaudhuri <inrcr27@gmail.com>
2024-08-10 17:50:26 +0530

    Added Color, Theme and Type

--- app/src/main/java/com/example/realtimeweatherapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/realtimeweatherapp/ui/theme/Type.kt
@@ -2,18 +2,32 @@ package com.example.realtimeweatherapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.realtimeweatherapp.R
 
+val AbrilFatface = FontFamily(
+    Font(R.font.abrilfatface_regular)
+)
+val Cabin_Sketch= FontFamily(
+    Font(R.font.cabinsketch_bold,FontWeight.Bold),
+    Font(R.font.cabinsketch_regular,FontWeight.Normal)
+)
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Cabin_Sketch,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = Cabin_Sketch,
+        fontWeight = FontWeight.Bold,
+    ),
+    bodySmall = TextStyle(
+        fontFamily = AbrilFatface,
+        fontWeight = FontWeight.SemiBold
     )
     /* Other default text styles to override
     titleLarge = TextStyle(
